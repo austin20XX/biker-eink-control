9fad5f82478a820fdcf303945ce57c301ee85afc
austin_m <1austinm@protonmail.com>
2024-07-06 16:02:05 -0500

    button presses

--- Featherwing.h
+++ Featherwing.h
@@ -4,3 +4,7 @@
 #define SRAM_CS 6
 #define EPD_RESET -1  // can set to -1 and share with microcontroller Reset!
 #define EPD_SPI &SPI // primary SPI
+
+#define EPD_GPIO_A 2
+#define EPD_GPIO_B 3
+#define EPD_GPIO_C 4
--- eink-vest-sketch.ino
+++ eink-vest-sketch.ino
@@ -4,31 +4,59 @@
 #include "Featherwing.h"
 
 
-#define EPD_DC 10
-#define EPD_CS 9
-#define EPD_BUSY -1 // can set to -1 to not use a pin (will wait a fixed delay)
-#define SRAM_CS 6
-#define EPD_RESET -1  // can set to -1 and share with microcontroller Reset!
-#define EPD_SPI &SPI // primary SPI
 // 2.9" Tricolor IL0373 chipset
 ThinkInk_290_Tricolor_Z10 display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS,
 EPD_BUSY, EPD_SPI);
+
+int aState, bState, cState = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
+  pinMode(EPD_GPIO_A, INPUT_PULLUP);
+  pinMode(EPD_GPIO_B, INPUT_PULLUP);
+    pinMode(EPD_GPIO_C, INPUT_PULLUP);
+  Serial.println("Pin Init");
+
   //TRICOLOR is the default here but being explicit
   display.begin(THINKINK_TRICOLOR);
   display.clearBuffer();
+  //   Serial.println("Clear Buffer.");
+
   display.setTextSize(2);
   display.setCursor(0,0);
-  display.setTextColor(EPD_RED);
+  display.setTextColor(EPD_BLACK);
   display.setTextWrap(true);
-  display.print("Austin Myers");
+  // display.print("Austin Myers1");
+  //   Serial.println("Print.");
   display.display();
+    Serial.println("Setup Complete.");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  aState = digitalRead(EPD_GPIO_A);
+  bState = digitalRead(EPD_GPIO_B);
+  cState = digitalRead(EPD_GPIO_C);
+  //It is pulled up HIGH
+   if (aState == LOW) {
+    Serial.println("A");
+    display.print("Austin Myers A");  
+    display.display();
+  }
+  else if (bState == LOW) {
+    Serial.println("B");
+    display.print("Austin Myers B");
+    display.display();
+  }
+  else if (cState == LOW) {
+    Serial.println("C");
+    display.print("Austin Myers C");
+    display.display();
+  }
+  //Slight delay for debouncing? Likely better ways... 
+  //DEBOUNCE LIBRARY! bounce2, button
+  delay(100);
 }
